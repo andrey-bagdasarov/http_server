b02ceb7deb89e71bf1e1da2cc28c077035aa579b
Andrey Bagdasarov <25719084+andrey-bagdasarov@users.noreply.github.com>
2021-11-20 00:54:28 +0200

    Add server struct and impl

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,19 @@ fn main() {
     let server = Server::new("127.0.0.1:8080");
     server.run();
 }
- 
+
+struct Server {
+    addres: String,
+}
+
+impl Server {
+    fn new(address: String) -> Self {
+        Server {
+            addres: address
+        }
+    }
+
+    fn run(self){
+        println!("Server is started")
+    }
+}
